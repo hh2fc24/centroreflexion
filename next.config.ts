76cde55d2e0d6ec74207010aa6666613340b7294
hh2fc24 <hh2fc24@gmail.com>
2026-05-11 17:17:17 -0400

    Update column images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   async headers() {
     return [
       {
